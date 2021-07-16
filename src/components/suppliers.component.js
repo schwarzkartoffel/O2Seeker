fc61a1f2451151e0dd32c3431014dac207311904
Rohitaswa Pradhan <rohitaswa96@gmail.com>
2021-07-16 14:05:14 +0530

    Initial implementation of All suppliers page

--- src/components/suppliers.component.js
+++ src/components/suppliers.component.js
@@ -1,11 +1,52 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Table } from "react-bootstrap";
+import axios from "axios";
 
 export default class Suppliers extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            suppliers: []
+        };
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/suppliers/')
+            .then(res => {
+                if (res.data.length > 0) {
+                    this.setState({
+                        suppliers: res.data
+                    });
+                }
+            })
+            .catch(err => console.log(err));
+    }
+
     render() {
         return (
             <Container className="mt-3">
-                <h1>This is the Suppliers Page</h1>
+                <h3>All suppliers</h3>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Supplier Name</th>
+                            <th>Phone Number</th>
+                            <th>Pin Code</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.suppliers.map((supplier, index) =>
+                            <tr key={supplier._id}>
+                                <td>{index}</td>
+                                <td>{supplier.supplierName}</td>
+                                <td>{supplier.phoneNumber}</td>
+                                <td>{supplier.pinCode}</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </Table>
             </Container>
         );
     }
