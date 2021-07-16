d67c9f627fccf5cc9a2334bcb2f695c85e2dc75a
Rohitaswa Pradhan <rohitaswa96@gmail.com>
2021-07-16 12:42:42 +0530

    Basic implementation of Add supplier page

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -4,7 +4,7 @@ import { Container } from "react-bootstrap";
 export default class Home extends React.Component {
     render() {
         return (
-            <Container>
+            <Container className="mt-3">
                 <h1>This is the Home Page</h1>
             </Container>
         );
--- src/components/suppliers.component.js
+++ src/components/suppliers.component.js
@@ -4,7 +4,7 @@ import { Container } from "react-bootstrap";
 export default class Suppliers extends React.Component {
     render() {
         return (
-            <Container>
+            <Container className="mt-3">
                 <h1>This is the Suppliers Page</h1>
             </Container>
         );
