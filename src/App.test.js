07263b605e56a6dfa13a8b8d5b985248ac427bb0
Rohitaswa Pradhan <rohitaswa96@gmail.com>
2021-07-24 14:04:57 +0530

    tested build badge

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Search for a something/i);
+  const linkElement = screen.getByText(/Search for a Supplier/i);
   expect(linkElement).toBeInTheDocument();
 });
