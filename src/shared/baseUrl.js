d1d7ab5bead1d57fffd50b6398dfaa8668ac2b58
Rohitaswa Pradhan <rohitaswa96@gmail.com>
2021-08-10 16:32:34 +0530

    reverted back to previous commit due to errors in separating build and prod environments

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,4 +1,2 @@
-const env = process.env.REACT_APP_ENVIRONMENT || "production";
-
-export const baseUrl = (env === "production") ? "https://o2seeker.herokuapp.com" : "http://localhost:3000";
-export const backendUrl = (env === "production") ? "https://o2seeker-backend.herokuapp.com" : "http://localhost:5000";
+export const baseUrl = "https://o2seeker.herokuapp.com"
+export const backendUrl = "https://o2seeker-backend.herokuapp.com"
